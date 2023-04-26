3636c3bf804ec44ab7a6b08e907a18f6ec8d5a18
kawla95 <kawlamansur@Kawla-Mansurs-MacBook-Pro.local>
2023-04-26 10:48:29 +0100

    added a data property called buttonDisabled

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,48 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+<h1> Kawla's Reaction Timer</h1>
+<button @click="start" :disabled="buttonDisabled" >play </button>
+<Block v-if="isPlaying" :delay="delay" @end="endGame"/>
+<p v-if="showResults"> Reaction time: {{ score  }} ms</p>
+
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Block from './components/Block.vue'
+import Results from './components/Results.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Block,
+    Results, 
+  },
+  data () {
+    return {
+      isPlaying: false, 
+      delay: null, 
+      score: null, 
+      showResults: false,
+      buttonDisabled: false
+    }
+
+  },
+  methods: {
+    start() {
+      // here we've set the delay to be between 2000 - 5000 ms. Math.random() is a number between 0-1
+      this.delay = 2000 + Math.random() * 5000 
+      this.isPlaying = true
+      this.showResults = false 
+      this.buttonDisabled = true
+
+    },
+    endGame(reactionTime) {
+      this.score = reactionTime
+      this.isPlaying = false
+      this.showResults = true
+      this.buttonDisabled = false
+
+    }
+
   }
 }
 </script>
@@ -20,7 +53,7 @@ export default {
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
+  color: #444;
   margin-top: 60px;
 }
 </style>
